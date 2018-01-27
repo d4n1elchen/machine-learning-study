a20f396fa669a9cbf793cfbcd94f94f251e8070a
Daniel Chen <team6612@gmail.com>
2018-01-27 20:21:43 +0800

    set prnt=true

--- genetic-algorithm/GA.py
+++ genetic-algorithm/GA.py
@@ -197,7 +197,7 @@ class numGA(GA):
 
 if __name__ == "__main__":
     ga = numGA(100, 0.01, 1000) # population_size, probability_mutation, max_generation
-    ga.run()
+    ga.run(prnt=True)
 
     print("The best solution of this run is {}, at {}".format(ga.global_max_curr, ga.solution))
 
