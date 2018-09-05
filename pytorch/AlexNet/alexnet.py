f7ba5ba2307f98c28a5a8f2f5ccae130d5f2eea8
Daniel Chen <team6612@gmail.com>
2018-09-05 09:37:18 +0800

    Make multi-process worker work

--- pytorch/AlexNet/alexnet.py
+++ pytorch/AlexNet/alexnet.py
@@ -48,29 +48,98 @@ class AlexNet(nn.Module):
         x = x.view(-1, 256 * 6 * 6)
         x = self.classifier(x)
         return x
+
+def main():
+    #%% Load data
+    data_dir = '.\\tiny-imagenet-200'
     
-#%% Load data
-data_dir = '.\\tiny-imagenet-200'
-
-traindir = os.path.join(data_dir, 'train')
-valdir = os.path.join(data_dir, 'val')
-augmentation = transforms.Compose([
-                   transforms.RandomResizedCrop(224),
-                   transforms.RandomHorizontalFlip(),
-                   transforms.ToTensor(),
-                   transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                        std=[1, 1, 1])
-               ])
-
-train_dataset = datasets.ImageFolder(traindir, augmentation)
-val_dataset = datasets.ImageFolder(valdir, augmentation)
-
-#%% Prepare dataloader
-batch_size = 128
-train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
-val_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=batch_size, shuffle=False, pin_memory=True)
+    traindir = os.path.join(data_dir, 'train')
+    valdir = os.path.join(data_dir, 'val')
+    augmentation = transforms.Compose([
+                       transforms.RandomResizedCrop(224),
+                       transforms.RandomHorizontalFlip(),
+                       transforms.ToTensor(),
+                       transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                            std=[1, 1, 1])
+                   ])
+    
+    train_dataset = datasets.ImageFolder(traindir, augmentation)
+    val_dataset = datasets.ImageFolder(valdir, augmentation)
+    
+    #%% Prepare dataloader
+    batch_size = 128
+    train_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=batch_size, shuffle=True, 
+            num_workers=4, pin_memory=True)
+    val_loader = torch.utils.data.DataLoader(
+            val_dataset, batch_size=batch_size, shuffle=False,
+            num_workers=4, pin_memory=True)
+    
+    #%% Init network
+    model = AlexNet()
+    
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), 0.01,
+                                momentum=0.9,
+                                weight_decay=0.0005)
+    
+    is_gpu = torch.cuda.is_available()
+    device = torch.device("cuda:0" if is_gpu else "cpu")
+    if is_gpu:
+        model.to(device)
+        criterion.to(device)
+        
+    #%% Training
+    epochs = 200
+    for epoch in range(epochs):
+    
+        batch_time = AverageMeter()
+        data_time = AverageMeter()
+        losses = AverageMeter()
+        top1 = AverageMeter()
+        top5 = AverageMeter()
+        
+        model.train()
+        
+        end = time.time()
+        
+        for i, (input, target) in enumerate(train_loader):
+            data_time.update(time.time() - end)
+            
+            # Move input and target to GPU if abailable
+            if is_gpu:
+                input, target = input.to(device), target.to(device)
+            
+            # Feed forward
+            output = model(input)
+            loss = criterion(output, target)
+            
+            # Metrics
+            prec1, prec5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), input.size(0))
+            top1.update(prec1[0], input.size(0))
+            top5.update(prec5[0], input.size(0))
+            
+            # Backprop and SGD
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+            
+            if i % 100 == 0:
+                print('Epoch: [{0}][{1}/{2}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                      'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                       epoch, i, len(train_loader), batch_time=batch_time,
+                       data_time=data_time, loss=losses, top1=top1, top5=top5))
+                
+        validate(val_loader, model, criterion, is_gpu, device)
 
 #%% Some helping function
 def accuracy(output, target, topk=(1,)):
@@ -89,7 +158,7 @@ def accuracy(output, target, topk=(1,)):
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
 
-def validate(val_loader, model, criterion, is_gpu):
+def validate(val_loader, model, criterion, is_gpu, device):
     losses = AverageMeter()
     top1 = AverageMeter()
     top5 = AverageMeter()
@@ -139,69 +208,6 @@ def adjust_learning_rate(optimizer, epoch):
     lr = 0.1 * (0.1 ** (epoch // 30))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
-
-#%% Init network
-model = AlexNet()
-
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), 0.1,
-                            momentum=0.9,
-                            weight_decay=0.0005)
-
-is_gpu = torch.cuda.is_available()
-device = torch.device("cuda:0" if is_gpu else "cpu")
-if is_gpu:
-    model.to(device)
-    criterion.to(device)
-    
-#%% Training
-epochs = 200
-for epoch in range(epochs):
-
-    batch_time = AverageMeter()
-    data_time = AverageMeter()
-    losses = AverageMeter()
-    top1 = AverageMeter()
-    top5 = AverageMeter()
-    
-    model.train()
-    
-    end = time.time()
-    
-    for i, (input, target) in enumerate(train_loader):
-        data_time.update(time.time() - end)
         
-        # Move input and target to GPU if abailable
-        if is_gpu:
-            input, target = input.to(device), target.to(device)
-        
-        # Feed forward
-        output = model(input)
-        loss = criterion(output, target)
-        
-        # Metrics
-        prec1, prec5 = accuracy(output, target, topk=(1, 5))
-        losses.update(loss.item(), input.size(0))
-        top1.update(prec1[0], input.size(0))
-        top5.update(prec5[0], input.size(0))
-        
-        # Backprop and SGD
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
-        
-        if i % 100 == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                   epoch, i, len(train_loader), batch_time=batch_time,
-                   data_time=data_time, loss=losses, top1=top1, top5=top5))
-            
-    validate(val_loader, model, criterion, is_gpu)
+if __name__ == '__main__':
+    main()
