4c38f26cbb764e5c058d9ffd52388287be289ff1
Daniel Chen <team6612@gmail.com>
2018-09-05 10:38:01 +0800

    Implement lr adjustment

--- pytorch/AlexNet/alexnet.py
+++ pytorch/AlexNet/alexnet.py
@@ -15,7 +15,7 @@ import torchvision.datasets as datasets
 
 #%% Define network
 class AlexNet(nn.Module):
-    
+
     def __init__(self, num_classes=200):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
@@ -42,7 +42,7 @@ class AlexNet(nn.Module):
             nn.ReLU(),
             nn.Linear(4096, num_classes),
         )
-    
+
     def forward(self, x):
         x = self.features(x)
         x = x.view(-1, 256 * 6 * 6)
@@ -50,9 +50,9 @@ class AlexNet(nn.Module):
         return x
 
 def main():
-    #%% Load data
+    ## Load data
     data_dir = '.\\tiny-imagenet-200'
-    
+
     traindir = os.path.join(data_dir, 'train')
     valdir = os.path.join(data_dir, 'val')
     augmentation = transforms.Compose([
@@ -62,102 +62,112 @@ def main():
                        transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                             std=[1, 1, 1])
                    ])
-    
+
     train_dataset = datasets.ImageFolder(traindir, augmentation)
     val_dataset = datasets.ImageFolder(valdir, augmentation)
-    
-    #%% Prepare dataloader
+
+    ## Prepare dataloader
     batch_size = 128
     train_loader = torch.utils.data.DataLoader(
-            train_dataset, batch_size=batch_size, shuffle=True, 
+            train_dataset, batch_size=batch_size, shuffle=True,
             num_workers=4, pin_memory=True)
     val_loader = torch.utils.data.DataLoader(
             val_dataset, batch_size=batch_size, shuffle=False,
             num_workers=4, pin_memory=True)
-    
-    #%% Init network
+
+    ## Init network
     model = AlexNet()
-    
+
+    # Hyper parameters
+    lr = 0.01
+    momentum = 0.9
+    weight_decay = 0.0005
+
+    # loss and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), 0.01,
-                                momentum=0.9,
-                                weight_decay=0.0005)
-    
+    optimizer = torch.optim.SGD(model.parameters(), lr,
+                                momentum=momentum,
+                                weight_decay=weight_decay)
+
+    # Get GPU
     is_gpu = torch.cuda.is_available()
     device = torch.device("cuda:0" if is_gpu else "cpu")
     if is_gpu:
         model.to(device)
         criterion.to(device)
-        
-    #%% Training
+
+    ## Training
+    impv_prec1 = 0
+    prev_prec1 = 0
     epochs = 200
     for epoch in range(epochs):
-    
-        batch_time = AverageMeter()
-        data_time = AverageMeter()
-        losses = AverageMeter()
-        top1 = AverageMeter()
-        top5 = AverageMeter()
-        
-        model.train()
-        
+        train(train_loader, model, criterion, is_gpu, device)
+        prec1 = validate(val_loader, model, criterion, is_gpu, device)
+
+        impv_prec1 += prec1 - prev_prec1
+        prev_prec1 = prec1
+
+        # Check acc improvement every 10 epoch
+        if epoch % 10 == 9:
+            if impv_prec1 < 1.0:
+                lr /= 10
+                for param_group in optimizer.param_groups:
+                    param_group['lr'] = lrdjust_learning_rate(init_learning_rate, optimizer, epoch)
+            print('EPOCH [{0}], Prec1 imporve: {1:.3f}, lr: {2:.4f}'.format(epoch, impv_prec, lr))
+            impv_prec1 = 0
+
+#%% Training
+def train(train_loader, model, criterion, is_gpu, device):
+    # Profiler
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+
+    # Metrics
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    model.train()
+
+    end = time.time()
+    for i, (input, target) in enumerate(train_loader):
+        data_time.update(time.time() - end)
+
+        # Move input and target to GPU if abailable
+        if is_gpu:
+            input, target = input.to(device), target.to(device)
+
+        # Feed forward
+        output = model(input)
+        loss = criterion(output, target)
+
+        # Metrics
+        prec1, prec5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), input.size(0))
+        top1.update(prec1[0], input.size(0))
+        top5.update(prec5[0], input.size(0))
+
+        # Backprop and SGD
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Profiling
+        batch_time.update(time.time() - end)
         end = time.time()
-        
-        for i, (input, target) in enumerate(train_loader):
-            data_time.update(time.time() - end)
-            
-            # Move input and target to GPU if abailable
-            if is_gpu:
-                input, target = input.to(device), target.to(device)
-            
-            # Feed forward
-            output = model(input)
-            loss = criterion(output, target)
-            
-            # Metrics
-            prec1, prec5 = accuracy(output, target, topk=(1, 5))
-            losses.update(loss.item(), input.size(0))
-            top1.update(prec1[0], input.size(0))
-            top5.update(prec5[0], input.size(0))
-            
-            # Backprop and SGD
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            # measure elapsed time
-            batch_time.update(time.time() - end)
-            end = time.time()
-            
-            if i % 100 == 0:
-                print('Epoch: [{0}][{1}/{2}]\t'
-                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                      'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                       epoch, i, len(train_loader), batch_time=batch_time,
-                       data_time=data_time, loss=losses, top1=top1, top5=top5))
-                
-        validate(val_loader, model, criterion, is_gpu, device)
 
-#%% Some helping function
-def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
+        # Print every 100 batch
+        if i % 100 == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   epoch, i, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1, top5=top5))
 
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
-
+#%% Validation
 def validate(val_loader, model, criterion, is_gpu, device):
     losses = AverageMeter()
     top1 = AverageMeter()
@@ -181,10 +191,27 @@ def validate(val_loader, model, criterion, is_gpu, device):
             top1.update(prec1[0], input.size(0))
             top5.update(prec5[0], input.size(0))
 
-        print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
+        print('VALIDATE Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
               .format(top1=top1, top5=top5))
 
     return top1.avg
+
+#%% Some helping function
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -202,12 +229,6 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
-        
-def adjust_learning_rate(optimizer, epoch):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = 0.1 * (0.1 ** (epoch // 30))
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-        
+
 if __name__ == '__main__':
     main()
