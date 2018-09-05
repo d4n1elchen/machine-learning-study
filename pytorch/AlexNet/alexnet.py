4e95e5a09a69cf146a79a95f13c8a16b7f79e2d6
Daniel Chen <team6612@gmail.com>
2018-09-05 14:36:15 +0800

    Add missing param for LRN

--- pytorch/AlexNet/alexnet.py
+++ pytorch/AlexNet/alexnet.py
@@ -21,11 +21,11 @@ class AlexNet(nn.Module):
         self.features = nn.Sequential(
             nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),
             nn.ReLU(),
-            nn.LocalResponseNorm(),
+            nn.LocalResponseNorm(5),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(96, 256, kernel_size=5, padding=2),
             nn.ReLU(),
-            nn.LocalResponseNorm(),
+            nn.LocalResponseNorm(5),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(256, 384, kernel_size=3, padding=1),
             nn.ReLU(),
