f90971c2a5627223795987cafd40bc318b4bdf21
Daniel Chen <team6612@gmail.com>
2018-11-11 14:08:06 +0800

    Fix test script

--- pytorch/GoogLeNet/test.py
+++ pytorch/GoogLeNet/test.py
@@ -12,7 +12,7 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 
-from google import GoogLeNet
+from googlenet import GoogLeNet
 
 #%% Load the model
 model = GoogLeNet()
@@ -49,13 +49,13 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 rand_id = np.random.randint(1000)
-image = Image.open(os.path.join('tiny-imagenet-200', 'test', 'images', 
+image = Image.open(os.path.join('tiny-imagenet-200', 'test', 'images',
                                'test_{}.JPEG'.format(rand_id)))
 plt.imshow(image)
 
 with torch.no_grad():
     output = model(val_transform(image).unsqueeze(0)).numpy()[0]
     top5_cate = output.argsort()[::-1][:5]
-    
+
     for i, cate in enumerate(top5_cate):
         print('PRED'+str(i+1)+': '+words[wnids[cate]])
