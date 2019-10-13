48b35792cc52975de2895956dc4b907b242d2857
Daniel Chen <team6612@gmail.com>
2019-10-12 22:12:31 -0700

    Add argtan version of cosine loss

--- pytorch/Extend-Loss/myloss.py
+++ pytorch/Extend-Loss/myloss.py
@@ -84,24 +84,81 @@ class CosineLoss2(Module):
             raise RuntimeError(f"Reduction: {self.reduction} does not support.")
         return output_reduced
 
+class CosineLoss3(Module):
+    r"""Returns cosine loss between :math:`input` (A 2D vector represent a point on the complex plane) and :math:`target` (Target angle in radius).
+
+    .. math ::
+        \text{loss} = cos(\theta_{\text{target}} - arg(input)).
+
+    Args:
+        eps (float, optional): Small value to avoid division by zero.
+            Default: 1e-8
+    Shape:
+        - Input1: :math:`(\ast_1, 2)`
+        - Input2: :math:`(\ast_1, 1)`
+        - Output: scalar
+    Examples::
+        >>> input1 = torch.randn(100, 2)
+        >>> input2 = torch.randn(100, 1)
+        >>> loss = myloss.CosineLoss3(eps=1e-6)
+        >>> output = loss(input1, input2)
+    """
+
+    def __init__(self, eps=1e-8, reduction='mean'):
+        super(CosineLoss3, self).__init__()
+        self.eps = eps
+        self.reduction = reduction
+
+    def forward(self, input, target):
+        if(len(input.shape) > 2):
+            if(input.shape[1] == 1 or input.shape[2] == 1):
+                input = input.view(-1, 2)
+        
+        if(len(target.shape) > 2):
+            if(target.shape[1] == 1 or target.shape[2] == 1):
+                target = target.view(-1, 2)
+
+        if(len(input.shape) > 2 or len(target.shape) > 2 or input.shape[1] != 2 or target.shape[1] != 1):
+            raise RuntimeError(f"Input should be a 2D vector and target should be a scalar but receive input = {input.shape} and target = {target.shape}")
+
+        re_input, im_input = torch.split(input, 1, dim=1)
+        arg_input = torch.atan2(re_input, im_input)
+        
+        output = torch.cos(target - arg_input)
+        
+        if self.reduction == 'mean':
+            output_reduced = output.mean()
+        else:
+            raise RuntimeError(f"Reduction: {self.reduction} does not support.")
+        return output_reduced
+
 if __name__=="__main__":
+    import numpy as np
     from torch.autograd import Variable
     from torch.nn import CosineSimilarity
 
     cos = CosineSimilarity()
     loss1 = CosineLoss(reduction="mean")
     loss2 = CosineLoss2(reduction="mean")
+    loss3 = CosineLoss3(reduction="mean")
 
     input1 = Variable(torch.Tensor([[2, 1], 
                                     [3, 4], 
                                     [4, 8],
                                     [3, 5], 
                                     [4, 7]]), requires_grad=True)
+
     input2 = torch.Tensor([[1, 2],
                           [3, 4],
                           [5, 2],
                           [2, 3],
                           [4, 5]])
+
+    input3 = torch.Tensor([[np.arctan2(1, 2)],
+                          [np.arctan2(3, 4)],
+                          [np.arctan2(5, 2)],
+                          [np.arctan2(2, 3)],
+                          [np.arctan2(4, 5)]])
 
     output = cos(input1, input2).mean()
     output.backward()
@@ -118,6 +175,12 @@ if __name__=="__main__":
     output2 = loss2(input1, input2)
     output2.backward()
     print(output2)
+    print(input1.grad)
+    input1.grad.zero_()
+
+    output3 = loss3(input1, input3)
+    output3.backward()
+    print(output3)
     print(input1.grad)
     input1.grad.zero_()
 
